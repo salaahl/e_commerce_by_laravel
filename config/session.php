6e572f39ae2d4a31d67ac8d401bf599c2f81b45b
salaahl <sokhona.salaha@gmail.com>
2023-04-18 02:43:03 +0200

    correctif

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN'),
+    'domain' => env('SESSION_DOMAIN', 'salaha-sokhona-ecommerce.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
