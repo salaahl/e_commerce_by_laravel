2e95fda689d2b3df54cde34b772a58dc7bba5b68
salaahl <67805036+salaahl@users.noreply.github.com>
2023-04-19 17:54:43 +0200

    Update read.blade.php

--- resources/views/CRUD/read.blade.php
+++ resources/views/CRUD/read.blade.php
@@ -9,27 +9,25 @@
 @section('main-content')
 <h1>Produits</h1>
 @foreach($products as $product)
-<div class="product">
-    <form class="form" method="GET" action="{{url('/CRUD/update')}}">
-        @csrf
-        <p class="field required">
-            <label>Catalogue :</label>
-            <input type="text" name="catalog" class="text-input" value="{{ $product['catalog'] }}" readonly>
-        </p>
-        <p class="field required">
-            <label>Référence :</label>
-            <input type="text" name="reference" class="text-input" value="{{ $product['reference'] }}" readonly>
-        </p>
-        <p class="field required">
-            <label>Nom :</label>
-            <input type="text" name="name" class="text-input" value="{{ $product['name'] }}" readonly>
-        </p>
-        <p class="field required">
-            <label for="product-stock">Stock :</label>
-            <input type="number" name="stock" class="text-input" value="{{ $product['stock'] }}">
-        </p>
-        <button class="button" type="submit">Mettre à jour le stock</button>
-    </form>
-</div>
+<form class="form" method="GET" action="{{url('/CRUD/update')}}">
+    @csrf
+    <p class="field required">
+        <label>Catalogue :</label>
+        <input type="text" name="catalog" class="text-input" value="{{ $product['catalog'] }}" readonly>
+    </p>
+    <p class="field required">
+        <label>Référence :</label>
+        <input type="text" name="reference" class="text-input" value="{{ $product['reference'] }}" readonly>
+    </p>
+    <p class="field required">
+        <label>Nom :</label>
+        <input type="text" name="name" class="text-input" value="{{ $product['name'] }}" readonly>
+    </p>
+    <p class="field required">
+        <label for="product-stock">Stock :</label>
+        <input type="number" name="stock" class="text-input" value="{{ $product['stock'] }}">
+    </p>
+    <button class="button" type="submit">Mettre à jour le stock</button>
+</form>
 @endforeach
 @endsection
