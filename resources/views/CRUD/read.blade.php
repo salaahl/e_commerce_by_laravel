c9ee38c70fbc1571f6e7435222718ec262ec62d0
salaahl <67805036+salaahl@users.noreply.github.com>
2023-04-19 15:14:17 +0200

    Update read.blade.php

--- resources/views/CRUD/read.blade.php
+++ resources/views/CRUD/read.blade.php
@@ -10,19 +10,26 @@
 <h2>Produits : </h2>
 @foreach($products as $product)
 <div class="product">
-    <span>Référence : {{ $product['reference'] }}</span>
-    <span>Nom : {{ $product['name'] }}</span>
-    <span>Catalogue : {{ $product['catalog'] }}</span>
-    <span>
-        <form method="GET" action="{{url('/CRUD/update')}}">
-            @csrf
+    <form method="GET" action="{{url('/CRUD/update')}}">
+        @csrf
+        <p class="field required">
+            <label>Catalogue :</label>
+            <input type="text" name="catalog" class="text-input" value="{{ $product['catalog'] }}" readonly>
+        </p>
+        <p class="field required">
+            <label>Référence :</label>
+            <input type="text" name="reference" class="text-input" value="{{ $product['reference'] }}" readonly>
+        </p>
+        <p class="field required">
+            <label>Nom :</label>
+            <input type="text" name="name" class="text-input" value="{{ $product['name'] }}" readonly>
+        </p>
+        <p class="field required">
             <label for="product-stock">Stock :</label>
-            <p class="field required">
-                <input type="number" id="product-stock" name="stock" class="text-input" value="{{ $product['stock'] }}">
-            </p>
-            <button type="submit">Mettre à jour le stock</button>
-        </form>
-    </span>
+            <input type="number" name="stock" class="text-input" value="{{ $product['stock'] }}">
+        </p>
+        <button type="submit">Mettre à jour le stock</button>
+    </form>
 </div>
 @endforeach
 @endsection
