486621bfa09bdd133cafcec4085d6a8b7c87db8e
salaahl <67805036+salaahl@users.noreply.github.com>
2023-04-19 13:29:59 +0200

    Update create.blade.php

--- resources/views/CRUD/create.blade.php
+++ resources/views/CRUD/create.blade.php
@@ -7,40 +7,41 @@
 @endsection
 
 @section('main-content')
-<form method="GET" id="form-dish" class="form" action="{{url('/CRUD/store')}}">
+<form method="GET" class="form" action="{{url('/CRUD/store')}}">
     @csrf
     <h1>Ajouter un nouveau catalogue :</h1>
     <p class="field required">
-        <input type="text" id="catalog" name="catalog" required="required" class="text-input" placeholder="Nom du catalogue">
+        <input type="text" id="catalog-name" name="name" required="required" class="text-input" placeholder="Nom du catalogue">
     </p>
     <p class="field">
         <input class="button" type="submit" value="Send">
     </p>
+    <input name="form" type="hidden" value="catalog">
 </form>
 
-<form method="GET" id="form-dish" class="form" action="{{url('/CRUD/store')}}">
+<form method="GET" class="form" action="{{url('/CRUD/store')}}">
     @csrf
     <h1>Ajouter un nouveau produit :</h1>
     <p class="field required">
-        <input type="text" id="product_reference" name="product_reference" required="required" class="text-input" placeholder="Reference du produit">
+        <input type="text" id="product-reference" name="reference" required="required" class="text-input" placeholder="Reference du produit">
     </p>
     <p class="field required">
-        <input type="text" id="product_name" name="product_name" required="required" class="text-input" placeholder="Nom du produit">
+        <input type="text" id="product-name" name="name" required="required" class="text-input" placeholder="Nom du produit">
     </p>
     <p class="field required">
-        <textarea name="product_description" required="required" class="textarea" placeholder="100 caractères maximum."></textarea>
+        <textarea name="description" required="required" class="textarea" placeholder="255 caractères maximum."></textarea>
     </p>
     <p class="field required">
-        <input type="text" id="product_price" name="product_price" required="required" inputmode="decimal" class="text-input" placeholder="9.99">
+        <input type="text" id="product-price" name="price" required="required" inputmode="decimal" class="text-input" placeholder="9.99">
     </p>
     <p class="field required">
-        <input type="number" id="product_stock" name="product_stock" class="text-input" placeholder="Stock">
+        <input type="number" id="product-stock" name="stock" class="text-input" placeholder="Stock">
     </p>
     <div class="field required">
         <label class="label">Type</label>
         <ul class="checkboxes">
             <li class="checkbox">
-                <select id="product_catalog" class="text-input" name="product_catalog">
+                <select id="product-catalog" class="text-input" name="catalog">
                     <option value="Homme">Homme</option>
                     <option value="Femme">Femme</option>
                     <option value="Enfant">Enfant</option>
@@ -50,10 +51,11 @@
         </ul>
     </div>
     <p class="field required">
-        <input type="file" id="product_picture" name="product_picture" accept="image/png, image/jpeg, image/svg">
+        <input type="file" id="product-picture" name="picture" accept="image/png, image/jpeg, image/svg">
     </p>
     <p class="field">
         <input class="button" type="submit" value="Send">
     </p>
+    <input name="form" type="hidden" value="product">
 </form>
 @endsection
