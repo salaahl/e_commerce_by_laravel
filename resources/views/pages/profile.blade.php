d7dfb1df384b35c997011977a7d2712dc78ca2a5
salaahl <67805036+salaahl@users.noreply.github.com>
2023-05-03 17:11:00 +0200

    Update profile.blade.php

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -25,6 +25,9 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 text-gray-900">
                   Historique de mes commandes :
+                  @foreach order as orders
+                    <p> {{ orders }} </p>
+                  @endforeach
               </div>
           </div>
       </div>
