07c2de3f347d728231cb6da8e7838fbaac8bd13d
salaahl <67805036+salaahl@users.noreply.github.com>
2023-07-29 16:36:37 +0200

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return Redirect::intended();
+                return redirect()->intended();
             }
         }
 
