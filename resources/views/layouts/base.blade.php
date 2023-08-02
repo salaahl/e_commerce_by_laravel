d8c871815d63c8a78434e756c8b80940d6ddef0f
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-02 17:25:10 +0200

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.css" rel="stylesheet" />
     <link href="{{ asset('css/normalize.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet" type="text/css" />
 </head>
@@ -25,6 +26,7 @@
     @yield('main-content')
     @include('layouts.footer')
     <script src="{{ asset('js/active_page.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.js"></script>
 </body>
 
 </html>
