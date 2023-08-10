ca42dee745d88a1138322e0f71b0f764a29d9420
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-10 13:52:22 +0200

    Update profile.blade.php

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -10,36 +10,34 @@
 <h1>Mon profil</h1>
 <main>
     <div class="mx-auto">
-        <div class="overflow-hidden shadow-sm sm:rounded-lg mb-4">
+        <aside class="overflow-hidden shadow-sm sm:rounded-lg mb-4">
             <div class="profile">
                 <a href="/profile">Modifier mes informations</a>
             </div>
-        </div>
-        <div class="overflow-hidden shadow-sm sm:rounded-lg">
-            <div>
-                Historique de mes commandes :
-                @foreach($bills as $bill)
-                <div class="bill">
-                    <h3>Facture numéro {{ $bill->id }}</h3>
+        </aside>
+        <section class="overflow-hidden shadow-sm sm:rounded-lg">
+            <p>Historique de mes commandes :</p>
+            @foreach($bills as $bill)
+            <article class="bill">
+                <h3>Facture numéro {{ $bill->id }}</h3>
+                <br>
+                Articles commandés :
+                <br>
+                @php($i = 0)
+                @foreach($orders as $order)
+                @if(isset($order[$i]->index_id) && $order[$i]->index_id === $bill->order_index)
+                <div class="article">
+                    - Référence : {{ $order[$i]->product_reference }}
                     <br>
-                    Articles commandés :
+                    - Quantité : {{ $order[$i]->quantity }}
                     <br>
-                    @php($i = 0)
-                    @foreach($orders as $order)
-                    @if(isset($order[$i]->index_id) && $order[$i]->index_id === $bill->order_index)
-                    <div class="article">
-                        - Référence : {{ $order[$i]->product_reference }}
-                        <br>
-                        - Quantité : {{ $order[$i]->quantity }}
-                        <br>
-                    </div>
-                    @php($i++)
-                    @endif
-                    @endforeach
                 </div>
+                @php($i++)
+                @endif
                 @endforeach
-            </div>
-        </div>
+            </article>
+            @endforeach
+        </section>
     </div>
 </main>
 @endsection
