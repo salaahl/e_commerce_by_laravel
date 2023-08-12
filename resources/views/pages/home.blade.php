83e1a1bb5571a420a06d1afda248de5ee515194f
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-12 10:23:16 +0200

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -113,7 +113,7 @@
     </section>
     <section class="coming-soon">
         <div class="section-text">
-            <h3>Nouvelle collection</h3>
+            <h2>Nouvelle collection</h2>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
                 magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
