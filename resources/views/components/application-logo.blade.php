0d1877e62644a5851ef275ad4b9db0626d4658da
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-11-26 13:35:44 +0100

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,3 @@
 <a href="/" class="flex items-center">
     <img src="/images/ecommerce_logo.png" class="website-logo h-8 mr-3" alt="Website logo">
-    <span class="self-center text-2xl font-semibold whitespace-nowrap">e-commerce</span>
 </a>
