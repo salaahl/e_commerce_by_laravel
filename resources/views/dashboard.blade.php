d64bc76496b55b73299beed938ce53ace01cd58d
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-11-26 15:59:03 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,11 +9,17 @@
 @section('main-content')
 <main>
     <h1>Tableau de bord</h1>
-    <h3>
-        <a href="/orders">Mes commandes</a>
-    </h3>
-    <h3>
-        <a href="/profile">Modifier mes informations</a>
-    </h3>
+    <section>
+        <article>
+            <h3>
+                <a href="/orders">Mes commandes</a>
+            </h3>
+        </article>
+        <article>
+            <h3>
+                <a href="/profile">Modifier mes informations</a>
+            </h3>
+        </article>
+    </section>
 </main>
 @endsection
