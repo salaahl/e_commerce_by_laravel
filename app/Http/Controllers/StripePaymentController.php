462e1c7983c439ad09a84bc33a7986946ba1c60a
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-07 16:22:35 +0100

    Update StripePaymentController.php

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -49,7 +49,17 @@ class StripePaymentController extends Controller
 
             $checkout_session = $stripe->checkout->sessions->create([
                 'ui_mode' => 'embedded',
-                'line_items' => $items,
+                'line_items' => [
+                    'price_data' => [
+                        'product_data' => [
+                            'name' => 'Nom',
+                        ],
+                        // Prix (sans le séparateur, ex : 1000 = 10)
+                        'unit_amount' => 1900,
+                        'currency' => 'eur',
+                    ],
+                    'quantity' => 1,
+                ],
                 'mode' => 'payment',
                 'return_url' => $APP_URL . '/return',
             ]);
