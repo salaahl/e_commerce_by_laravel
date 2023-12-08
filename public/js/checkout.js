d29355d52343206852d0de6b0e05338187f562a2
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-08 10:40:59 +0100

    Update checkout.js

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -20,13 +20,30 @@ async function initialize() {
     const clientSecret =
         "pi_3OKrijBWdqqNqzCq14H4LfBL_secret_xT7NZ8sU9JKmjEels9lHab0vM";
 
-    elements = stripe.elements({ clientSecret });
-
-    const paymentElementOptions = {
-        layout: "tabs",
-    };
-
-    const paymentElement = elements.create("payment", paymentElementOptions);
+    // Customize the appearance of Elements using the Appearance API.
+    const appearance = { /* ... */ };
+    
+    // Enable the skeleton loader UI for the optimal loading experience.
+    const loader = 'auto';
+    
+    // Create an elements group from the Stripe instance, passing the clientSecret (obtained in step 2), loader, and appearance (optional).
+    const elements = stripe.elements({clientSecret, appearance, loader});
+    
+    // Create Element instances
+    const linkAuthenticationElement = elements.create("linkAuthentication");
+    // Passing in defaultValues is optional, but useful if you want to prefill consumer information to
+    // ease consumer experience.
+    const paymentElement = elements.create('payment', {
+      defaultValues: {
+        billingDetails: {
+          name: 'John Doe',
+          phone: '888-888-8888',
+        },
+      },
+    });
+    
+    // Mount the Elements to their corresponding DOM node
+    linkAuthenticationElement.mount("#link-authentication-element");
     paymentElement.mount("#payment-element");
 }
 
