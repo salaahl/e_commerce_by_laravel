9f53f79a4f41166842a7dd570f6e133607db040c
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-08 15:37:48 +0100

    Update StripePaymentController.php
    Revert

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -19,52 +19,6 @@ class StripePaymentController extends Controller
         return view('stripe/checkout');
     }
 
-    //
-    public function checkoutCustomPost(Request $request)
-    {
-        try {
-        $basket = Basket::where('user_email', auth()->user()->email)->get();
-        
-            if ($basket) {
-                $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
-                $amount = 0;
-    
-                foreach ($basket as $basket_item) {
-                    $product = Product::where('reference', $basket_item->product_reference)->first();
-                    $amount += filter_var($product->price * $basket_item->quantity, FILTER_SANITIZE_NUMBER_INT);
-                };
-        
-                header('Content-Type: application/json');
-        
-                // retrieve JSON from POST body
-                $jsonStr = file_get_contents('php://input');
-                $jsonObj = json_decode($jsonStr);
-    
-                // Create a PaymentIntent with amount and currency
-                $paymentIntent = $stripe->paymentIntents->create([
-                    'amount' => $amount,
-                    'currency' => 'eur',
-                ]);
-    
-                $output = [
-                    'clientSecret' => $paymentIntent->client_secret,
-                ];
-    
-                return response()->json([
-                    'clientSecret' => $output,
-                ]);
-            } else {
-                return redirect()->route('basket');
-            }
-        } catch (Exception $e) {
-            http_response_code(500);
-
-            return response()->json([
-                'error' => $e->getMessage(),
-            ]);
-        }
-    }
-
 
     //
     public function checkoutPost(Request $request)
@@ -74,19 +28,16 @@ class StripePaymentController extends Controller
             $basket = Basket::where('user_email', auth()->user()->email)->get();
 
             if ($basket) {
-                $total_price = 0;
                 $items = [];
 
                 foreach ($basket as $basket_item) {
                     $product = Product::where('reference', $basket_item->product_reference)->first();
-                    $total_price += $product->price * $basket_item->quantity;
                     $items[] = [
                         'price_data' => [
                             'product_data' => [
                                 'name' => $product->name,
                                 'images' => [$APP_URL . '/images/' . $product->picture],
                             ],
-                            // Prix (sans le séparateur, ex : 1000 = 10)
                             'unit_amount' => $product->price * 100,
                             'currency' => 'eur',
                         ],
@@ -104,9 +55,8 @@ class StripePaymentController extends Controller
                     'shipping_address_collection' => ['allowed_countries' => ['FR']],
                     'custom_text' => [
                         'shipping_address' => [
-                            'message' => 'Please note that we can\'t guarantee 2-day delivery for PO boxes at this time.',
+                            'message' => 'Comptez un délai de cinq jours ouvrés pour la livraison.',
                         ],
-                        'submit' => ['message' => 'We\'ll email you instructions on how to get started.'],
                     ],
                     'return_url' => $APP_URL . '/return',
                 ]);
