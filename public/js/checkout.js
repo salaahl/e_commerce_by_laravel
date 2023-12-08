94e720f5e194052b3a9cd87a587087f4900af839
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-08 11:15:11 +0100

    Update checkout.js

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -21,28 +21,41 @@ async function initialize() {
         "pi_3OKrijBWdqqNqzCq14H4LfBL_secret_xT7NZ8sU9JKmjEels9lHab0vM";
 
     // Customize the appearance of Elements using the Appearance API.
-    const appearance = { /* ... */ };
+    const appearance = {
+        theme: 'flat' 
+    };
     
     // Enable the skeleton loader UI for the optimal loading experience.
     const loader = 'auto';
     
     // Create an elements group from the Stripe instance, passing the clientSecret (obtained in step 2), loader, and appearance (optional).
     const elements = stripe.elements({clientSecret, appearance, loader});
-    
-    // Create Element instances
-    const linkAuthenticationElement = elements.create("linkAuthentication");
-    // Passing in defaultValues is optional, but useful if you want to prefill consumer information to
-    // ease consumer experience.
-    const paymentElement = elements.create('payment', {
+
+    const addressOptions = { mode: 'shipping' };
+    const paymentOptions = {
       defaultValues: {
         billingDetails: {
           name: 'John Doe',
           phone: '888-888-8888',
         },
       },
-    });
+      layout: {
+        type: 'accordion',
+        defaultCollapsed: false,
+        radios: false,
+        spacedAccordionItems: true
+      },
+    };
+    
+    // Create Element instances
+    const addressElement = elements.create('address', addressOptions);
+    const linkAuthenticationElement = elements.create("linkAuthentication");
+    // Passing in defaultValues is optional, but useful if you want to prefill consumer information to
+    // ease consumer experience.
+    const paymentElement = elements.create('payment', paymentOptions);
     
     // Mount the Elements to their corresponding DOM node
+    addressElement.mount("#address-element");
     linkAuthenticationElement.mount("#link-authentication-element");
     paymentElement.mount("#payment-element");
 }
