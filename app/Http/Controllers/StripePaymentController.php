61dc2a94b55df54b6be75ca28b107501970671d9
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-12 14:23:26 +0100

    Update StripePaymentController.php

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -159,10 +159,62 @@ class StripePaymentController extends Controller
         }
     }
 
-    public function return()
+    // Le CRSF de cette route a été désactivé, voir si c'est absolument nécessaire
+    public function webhooks()
     {
-        return view(
-            'stripe/return'
-        );
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        // Replace this endpoint secret with your endpoint's unique secret
+        // If you are testing with the CLI, find the secret by running 'stripe listen'
+        // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+        // at https://dashboard.stripe.com/webhooks
+        $endpoint_secret = env('STRIPE_SECRET_ENDPOINT');
+        
+        $payload = @file_get_contents('php://input');
+        $event = null;
+        
+        try {
+          $event = \Stripe\Event::constructFrom(
+            json_decode($payload, true)
+          );
+        } catch(\UnexpectedValueException $e) {
+          // Invalid payload
+          echo '⚠️  Webhook error while parsing basic request.';
+          http_response_code(400);
+          exit();
+        }
+        if ($endpoint_secret) {
+          // Only verify the event if there is an endpoint secret defined
+          // Otherwise use the basic decoded event
+          $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+          try {
+            $event = \Stripe\Webhook::constructEvent(
+              $payload, $sig_header, $endpoint_secret
+            );
+          } catch(\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            echo '⚠️  Webhook error while validating signature.';
+            http_response_code(400);
+            exit();
+          }
+        }
+        
+        // Handle the event
+        switch ($event->type) {
+          case 'payment_intent.succeeded':
+            $paymentIntent = $event->data->object; // contains a \Stripe\PaymentIntent
+            // Then define and call a method to handle the successful payment intent.
+            // handlePaymentIntentSucceeded($paymentIntent);
+            break;
+          case 'payment_method.attached':
+            $paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
+            // Then define and call a method to handle the successful attachment of a PaymentMethod.
+            // handlePaymentMethodAttached($paymentMethod);
+            break;
+          default:
+            // Unexpected event type
+            error_log('Received unknown event type');
+        }
+        
+        http_response_code(200);
     }
 }
