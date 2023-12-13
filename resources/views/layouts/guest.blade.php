9f7a8888c78f9bd4d718af482d70af0389d2c420
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-13 17:07:46 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
 @endsection
 
 @section('header')
-@endsection
+@show
                 
 @section('main-content')
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
