dec070e86f3242f48777cbaaf87849e8404d0c1f
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-13 17:25:30 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,9 +8,6 @@
 <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @endsection
-
-@section('header')
-@endsection
                 
 @section('main-content')
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
@@ -24,7 +21,4 @@
         {{ $slot }}
     </div>
 </div>
-@endsection
-
-@section('footer')
 @endsection
