daab00eebe70e5810df0c70656497b6d4cd14ff4
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2023-12-14 18:57:26 +0100

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -21,13 +21,13 @@
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-500 shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-password-form')
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-500 shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.delete-user-form')
             </div>
